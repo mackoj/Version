e1748fc74e2553fcd184a55b149a8bf2fad266eb
Jeffrey Macko <mackoj@users.noreply.github.com>
2019-11-04 16:12:11 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,11 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Version",
-            path: "Version"),
-        .testTarget(
-            name: "VersionTests",
-            dependencies: ["Version"],
-            path: "Version"
-        ),
+            path: "Version")
     ]
 )
